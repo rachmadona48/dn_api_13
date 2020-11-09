d9df36c8ece1f6ac37d00fe2682ae8a6ae7ef134
rachmadona48 <rayci232@gmail.com>
2020-11-09 11:34:21 +0700

    revisi

--- dn_api_13/__manifest__.py
+++ dn_api_13/__manifest__.py
@@ -3,16 +3,22 @@
     'name': "Odoo 13 Rest Api (Json)",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        This module to akses odoo via rest api with dynamic concept""",
 
     'description': """
+        Module to integration odoo with other system, to using this module you bust be setting in odoo.conf
+        
         1) Open odoo.conf
         2) Add 'dbfilter = dbname'.
         3) Restart odoo service.
+        4) Test api with tools (ex: postman).
+
+    Any questions?
+    Want to test this module?
+    Email me: nra.junior26@gmail.com 
     """,
 
-    'author': "R4Y Jr",
+    'author': "NRA",
     'website': "-",
 
     # Categories can be used to filter modules in modules listing
@@ -22,7 +28,8 @@
     'price': 100,
     'currency': 'EUR',
     'version': '13.0',
-    'support': 'rayci232@gmail.com',
+    'support': 'nra.junior26@gmail.com ',
+    'images': ['images/login1.PNG'],
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
